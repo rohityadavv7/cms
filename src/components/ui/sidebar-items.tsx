39ef646ffa88f0f0b21dcaf5710c30fa7e0fedd9
Rohit <rohityadav@Rohits-MacBook-Air.local>
2025-05-08 13:26:19 +0530

    fixed menu bar animation

--- src/components/ui/sidebar-items.tsx
+++ src/components/ui/sidebar-items.tsx
@@ -8,6 +8,8 @@ import {
   TooltipTrigger,
 } from './tooltip';
 import { usePathname } from 'next/navigation';
+import {motion} from "motion/react"
+import { easeIn } from 'framer-motion';
 
 export const SidebarItems = ({
   items,
@@ -32,22 +34,31 @@ export const SidebarItems = ({
           <TooltipProvider key={item.id}>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Link
-                  href={item.href}
-                  onClick={() => setIsCollapsed(true)}
-                  className={`flex items-center rounded-lg p-3 text-center transition-all duration-300 ${
-                    isActive
-                      ? 'bg-blue-600/15 text-blue-500'
-                      : 'hover:bg-blue-600/5 hover:text-blue-500'
-                  } ${isCollapsed ? 'justify-center' : 'gap-2'}`}
+                <motion.div
+                initial={{opacity:0, y:0,filter:"blur(10px)"}}
+                animate={{opacity:1, y:0,filter:"blur(0px)"}}
                 >
-                  <item.Component />
-                  {!isCollapsed && (
-                    <span className="text-lg font-medium tracking-tight">
-                      {item.name}
-                    </span>
-                  )}
-                </Link>
+                  <Link
+                    href={item.href}
+                    onClick={() => setIsCollapsed(true)}
+                    className={`flex items-center rounded-lg p-3 text-center transition-all duration-300 ${
+                      isActive
+                        ? 'bg-blue-600/15 text-blue-500'
+                        : 'hover:bg-blue-600/5 hover:text-blue-500'
+                    } ${isCollapsed ? 'justify-center' : 'gap-2'}`}
+                  >
+                    <item.Component />
+                    {!isCollapsed && (
+                      <motion.span 
+                      initial={{opacity:0, y:0,filter:"blur(10px)"}}
+                      animate={{opacity:1, y:0,filter:"blur(0px)"}}
+                      transition={{duration:0.3, ease:easeIn}}
+                      className="text-lg font-medium tracking-tight">
+                        {item.name}
+                      </motion.span>
+                    )}
+                  </Link>
+                </motion.div>
               </TooltipTrigger>
               {isCollapsed && (
                 <TooltipContent side="right" sideOffset={4}>
